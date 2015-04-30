e1d715c63acab5a7e5f5800feba8ebebac144f08
Jasper Meyer <jasper.meyer@live.de>
2015-04-30 13:25:29 +0200

    added bootstrap to installation db form

--- server/install/formDb.php
+++ server/install/formDb.php
@@ -7,6 +7,14 @@
 
 <html>
     <head>
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="bootstrap/css/bootstrap-theme.min.css">
+
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="bootstrap/js/bootstrap.min.js"></script>
     </head>
     <body>
 
